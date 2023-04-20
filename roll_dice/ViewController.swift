302a390dcf2b87ce4d846cf93e3263164620656e
Abibulla Kurbanov <tenerifallubiba@yahoo.com>
2023-04-20 17:21:40 +0600

    Initial Commit

---
+++ roll_dice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  roll_dice
+//
+//  Created by abibulla on 20.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
